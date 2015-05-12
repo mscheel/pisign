e8e43c72871303d50c5e9fdc5c2d1f3ceb87c983
Mark Scheel <mscheel@digitalconstruction.com>
2015-05-11 22:06:29 -0600

    retry original sample code with serial, after ruby upgrade to 2.1.1 and gem install x 3

--- hello.rb
+++ hello.rb
@@ -2,19 +2,34 @@
 
 require 'rubygems'
 require 'betabrite'
-require 'usb'
+require 'serialport'
 
-bb=BetaBrite::USB.new do |sign|
+bb=BetaBrite::Serial.new('/dev/ttyUSB0') do |sign|
 
- sign.stringfile('0') do
-   print string("Hello").green
- end
+  bb = BetaBrite::Serial.new('/dev/ttyUSB0') do |sign|
+    sign.allocate do |memory|
+      memory.text('A', 4096)
+      memory.string('0', 64)
+      memory.dots('1', 7, 7)
+    end
+  end
+  bb.write_memory!
 
- sign.textfile do
-   flash
-   print stringfile('0')
-   print string(" Blue").rgb('0000FF')
- end
+  puts "memmory allocated"
+
+  BetaBrite::Serial.new('/dev/ttyUSB0').clear_memory!
+
+  puts "sign reset"
+
+bb = BetaBrite::Serial.new('/dev/ttyUSB0') do |sign|
+    sign.textfile do
+      print left_arrow + string("Hello ").green
+      print string(" world.").green + sail_boat
+    end
+  end
+  bb.write!
+
+  puts "written"
 end
 
 bb.write!
