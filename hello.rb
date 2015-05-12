c74e922a6673f9ce280eb0ffeeb1c580aa2dc944
Mark Scheel <mscheel@digitalconstruction.com>
2015-05-11 20:11:52 -0600

    try to control sign, but has dependency problems

--- hello.rb
+++ hello.rb
@@ -1,5 +1,32 @@
 #!/usr/bin/ruby
 
-# Hello world ruby program
+require 'rubygems'
+require 'betabrite'
+require 'usb'
 
-puts "Hello World!";
+# BetaBrite Examples
+
+## Allocate Memory
+
+  bb = BetaBrite::Serial.new('/dev/ttyUSB0') do |sign|
+    sign.allocate do |memory|
+      memory.text('A', 4096)
+      memory.string('0', 64)
+      memory.dots('1', 7, 7)
+    end
+  end
+  bb.write_memory!
+
+## Reset the sign
+
+  BetaBrite::Serial.new('/dev/ttyUSB0').clear_memory!
+  
+## Write a text file
+
+  bb = BetaBrite::Serial.new('/dev/ttyUSB0') do |sign|
+    sign.textfile do
+      print left_arrow + string("Hello ").green
+      print string(" world.").green + sail_boat
+    end
+  end
+  bb.write!
